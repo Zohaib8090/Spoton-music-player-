9ab0a5cd03deaad49c95c46b3854a307927c0993
zohaibbaig144 <zohaibbaig144@gmail.com>
2025-10-31 22:31:43 +0000

    fix: correct path alias and update import

--- app/context/PlaybackSettingsContext.tsx
+++ app/context/PlaybackSettingsContext.tsx
@@ -1,6 +1,6 @@
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import * as AudioService from '@/services/AudioService';
+import * as AudioService from '@/app/services/AudioService';
 
 const PlaybackSettingsContext = createContext();
 
