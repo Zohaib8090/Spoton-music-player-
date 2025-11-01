a33c78d6a82db60bd56de1fddaae6f0778dcc370
zohaibbaig144 <zohaibbaig144@gmail.com>
2025-11-01 15:12:09 +0000

    fix: add kotlin-bom to resolve build issue

--- plugins/withAndroidDuplicateClassesFix.js
+++ plugins/withAndroidDuplicateClassesFix.js
@@ -3,15 +3,12 @@ const { withAppBuildGradle } = require('@expo/config-plugins');
 const withAndroidDuplicateClassesFix = (config) => {
   return withAppBuildGradle(config, (config) => {
     if (config.modResults.language === 'groovy') {
-      config.modResults.contents = config.modResults.contents + `
-configurations.all {
-    exclude group: 'com.android.support', module: 'support-v4'
-    exclude group: 'com.android.support', module: 'support-compat'
-    resolutionStrategy {
-        force 'androidx.core:core-ktx:1.8.0'
-    }
-}
-`;
+      config.modResults.contents = config.modResults.contents.replace(
+        'dependencies {',
+        `dependencies {
+            implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+        `
+      );
     }
     return config;
   });
